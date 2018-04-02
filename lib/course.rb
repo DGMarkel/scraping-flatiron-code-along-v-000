7186fe905e4ff5a7dfa1002dea57931a3afa45bd
David Markel <davidgmarkel@gmail.com>
2018-04-02 14:59:59 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,15 +2,16 @@ class Course
 attr_accessor :title, :schedule, :description
 @@all = []
 
-def initialize
-  @@all << self
+  def initialize
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+    def self.all
+      @@all
+    end
 
-def self.reset_all
-  @@all.clear
-end
+    def self.reset_all
+      @@all.clear
+    end
 
 end
